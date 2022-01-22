da1f1ce99ed06b91ff96f1edd991e09d58292fb9
Gabrielubi <tronalddump775@gmail.com>
2022-01-21 22:55:05 -0300

    Format

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -7,19 +7,18 @@ module.exports = {
         if (msg.content === "c!test") {
             console.log("Test");
             msg.reply("Test");
-        }
+        };
 
         if (msg.attachments.first()) {
             catchAttachment(msg.attachments.first().url);
         };
     }
-},
+};
 
-
-    function catchAttachment(url) {
-        var timestamp = Math.floor(new Date().getTime() / 1000);
-        request.get(url)
-            .on('error', console.error)
-            .pipe(fs.createWriteStream("images/collateral/" + timestamp + '.png'))
-        console.log("I've got something from the server")
-    }
+function catchAttachment(url) {
+    var timestamp = Math.floor(new Date().getTime() / 1000);
+    request.get(url)
+        .on('error', console.error)
+        .pipe(fs.createWriteStream("images/collateral/" + timestamp + '.png'));
+    console.log("I've got something from the server");
+}
--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,9 @@
-const { DISCORD_CHANNEL_ID } = require ('../config.json')
+const { DISCORD_CHANNEL_ID } = require('../config.json')
 module.exports = {
 	name: 'ready',
 	once: false,
 	execute(bot) {
-		console.log(`\n${bot.user.tag} online\n------------------------` );
-        bot.user.setPresence({ activities: [{ name: 'Under development' }], status: 'Disconnected' });
-	},	
+		console.log(`\n${bot.user.tag} online\n------------------------`);
+		bot.user.setPresence({ activities: [{ name: 'Under development' }], status: 'Disconnected' });
+	},
 };
