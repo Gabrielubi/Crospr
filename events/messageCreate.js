e882c03dcb17dbc4586c6627fea2e305c08963dc
Gabrielubi <tronalddump775@gmail.com>
2022-01-26 04:15:39 -0300

    Now it recognizes different types of media

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -10,15 +10,66 @@ module.exports = {
         };
 
         if (msg.attachments.first()) {
-            catchAttachment(msg.attachments.first().url);
+            if (msg.author.bot || msg.channel.type === "dm") return
+
+            switch (msg.attachments.first().contentType) {
+                case 'image/jpeg':
+                case 'image/png':
+                case 'image/webp':
+                    catchAttachmentImage(msg.attachments.first().url , msg.attachments.first().contentType);
+                    break;
+
+                case 'image/gif':
+                    catchAttachmentGif(msg.attachments.first().url);
+                    break;
+
+                case 'video/mp4':
+                case 'video/webm':
+                    catchAttachmentVideo(msg.attachments.first().url, msg.attachments.first().contentType);
+                    break;
+
+                default:
+                    console.log(`There is an attachment but I cant recognize it`)
+            }
         };
     }
 };
 
-function catchAttachment(url) {
+function catchAttachmentImage(url , type) {
+    console.log("I've got a image from the server");
+    var timestamp = Math.floor(new Date().getTime() / 1000);
+    if (type === 'image/webp'){
+    request.get(url)
+        .on('error', console.error)
+        .pipe(fs.createWriteStream('media/collateral/image/' + timestamp + '.webp'));
+    }else{
+        request.get(url)
+        .on('error', console.error)
+        .pipe(fs.createWriteStream('media/collateral/image/' + timestamp + '.png'));
+    }
+    console.log("> Image downloaded")
+}
+
+function catchAttachmentVideo(url , type) {
+    console.log("> I've got a video from the server");
+    var timestamp = Math.floor(new Date().getTime() / 1000);
+    if (type === 'video/webm'){
+    request.get(url)
+        .on('error', console.error)
+        .pipe(fs.createWriteStream('media/collateral/video/' + timestamp + '.webm'));
+    }else{
+        request.get(url)
+        .on('error', console.error)
+        .pipe(fs.createWriteStream('media/collateral/video/' + timestamp + '.mp4'));
+    }
+    console.log("> Video downloaded")
+}
+
+function catchAttachmentGif(url) {
+    console.log("> I've got a gif from the server");
     var timestamp = Math.floor(new Date().getTime() / 1000);
     request.get(url)
         .on('error', console.error)
-        .pipe(fs.createWriteStream("images/collateral/" + timestamp + '.png'));
-    console.log("I've got something from the server");
+        .pipe(fs.createWriteStream('media/collateral/gif/' + timestamp + '.gif'));
+        console.log("> Gif downloaded");
 }
--- events/twitter.js
+++ events/twitter.js
@@ -16,60 +16,55 @@ module.exports = {
             strictSSL: true,
         });
 
+        //imagen 1459754937450057731    type photo
+        //gif  1454225217539223560      type animated_gif
+        //vidio 1483352525738708992     type video
+
         //  follow:[] can be an array of users ID.
         let stream = T.stream('statuses/filter', { follow: [TWITTER_USER_ID], tweet_mode: 'extended' })
         const dest = DISCORD_CHANNEL_ID;
-        
+
+
+
         try {
             stream.on('tweet', function (tweet) {
                 //For some reason a retweeted message counts as a tweet. The 'tweet ID' is the only thing that really matters
-                const twitterMessage = `https://twitter.com/Crospr/status/${tweet.id_str}`
+                const twitterMessage = `https://twitter.com/Crospr/status/${tweet.id_str}`;
                 bot.channels.cache.get(dest).send(twitterMessage);
                 let timestamp = Date.now();
-                console.log(`-------New tweet!-------\n> ${new Date(timestamp).toLocaleTimeString()}`)
+                console.log(`-------New tweet!-------\n> ${new Date(timestamp).toLocaleTimeString()}`);
 
-                if (tweet.entities.media == undefined) {
-                    console.log("Can't find images... yet");
-                    T.get('statuses/show/:id', { id: tweet.id_str, tweet_mode: 'extended' }, function (data) {
-                        if (data.retweeted_status.extended_entities !== undefined) {
-                            let index = 0;
-                            console.log("Here they are");
-                            for (var j of data.retweeted_status.extended_entities.media) {
-                                let i = data.retweeted_status.extended_entities.media[index].media_url;
-                                let dir = "images/" + tweet.id_str + index + ".png";
-                                let path = 'disk:/Crospr/' + tweet.id_str + index + ".png";
-                                catchTw(i, dir);
-                                uploadTw(i, path);
-                                index++
-                            }
-                        } else {
-                            console.log("The tweet doesn't have images");
+                T.get('statuses/show/:id', { id: tweet.id_str, tweet_mode: 'extended' }, function (err, data, response) {
+                    if (data.extended_entities !== undefined) {
+                        switch (data.extended_entities.media[0].type) {
+                            case "video":
+                                handleTwVideo(data)
+                                break;
+                            case "animated_gif":
+                                handleTwAnimatedGif(data)
+                                break;
+                            case "photo": 
+                                handleTwImage(data);
+                                break;
+                            default:
+                                console.log("Can't break it down");
+                                break;
                         }
-                    });
-
-                } else {
-                    let index = 0;
-                    for (var j of tweet.extended_entities.media) {
-                        let dir = "images/" + tweet.id_str + index + ".png";
-                        let i = tweet.extended_entities.media[index].media_url;
-                        let path = 'disk:/Crospr/' + tweet.id_str + index + ".png";
-                        catchTw(i, dir);
-                        uploadTw(i, path);
-                        index++;
+                    } else {
+                        console.log("The tweet doesn't have images");
                     }
-                }
+                });
             })
-        }
-        catch (e) {
+        } catch (e) {
             console.log("Here is the error : " + e);
         }
 
-        stream.on('error', (e) =>{
+        stream.on('error', (e) => {
             console.log(`${e}\nrestart in 40 seconds...`);
             //to do
         })
 
-        stream.on('uncaughtException' , (e) =>{
+        stream.on('uncaughtException', (e) => {
             console.log(`Exception : ${e}\nrestart in 40 seconds...`);
             //to do
         })
@@ -79,9 +74,50 @@ module.exports = {
 function uploadTw(url, path) {
     disk.upload.remoteFile(YANDEX_TOKEN, url, path);
     console.log("> Uploaded to cloud\n------------------------\n");
-}
+};
 
 function catchTw(url, filename) {
     request(url).pipe(fs.createWriteStream(filename));
     console.log("> Downloaded to local files")
 };
+
+function handleTwVideo(tweet) {
+    let bitrate = 0;
+    let video_url;
+    for (let j = 0; j < tweet.extended_entities.media[0].video_info.variants.length; j++) {
+        if (tweet.extended_entities.media[0].video_info.variants[j].bitrate) {
+            if (tweet.extended_entities.media[0].video_info.variants[j].bitrate > bitrate) {
+                bitrate = tweet.extended_entities.media[0].video_info.variants[j].bitrate;
+                video_url = tweet.extended_entities.media[0].video_info.variants[j].url;
+            }
+        }
+    }
+    let i = video_url;
+    let dir = "media/video/" + tweet.id_str + ".mp4";
+    let path = 'disk:/Crospr/video' + tweet.id_str + ".mp4";
+    catchTw(i, dir);
+    uploadTw(i, path);
+    //iterator? maybe
+};
+
+function handleTwAnimatedGif(tweet) {
+    let video_url = tweet.extended_entities.media[0].video_info.variants[0].url;
+    let i = video_url;
+    let dir = "media/animated_gif/" + tweet.id_str + ".mp4";
+    let path = 'disk:/Crospr/video' + tweet.id_str + ".mp4";
+    catchTw(i, dir);
+    uploadTw(i, path);
+    //iterator? not needed. Twitter only admits one gif per tweet
+};
+
+function handleTwImage(tweet) {
+    let index = 0;
+    for (var j of tweet.retweeted_status.extended_entities.media) {
+        let i = tweet.retweeted_status.extended_entities.media[index].media_url;
+        let dir = "media/images/" + tweet.id_str + index + ".png";
+        let path = 'disk:/Crospr/' + tweet.id_str + index + ".png";
+        catchTw(i, dir);
+        uploadTw(i, path);
+        index++;
+    };
+}
